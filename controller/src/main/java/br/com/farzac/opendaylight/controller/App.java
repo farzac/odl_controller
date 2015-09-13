4e752a8f8ff247016f20806c6c59db6cc786fdf3
farzac <zaccantte@gmail.com>
2015-09-13 01:20:09 -0300

    Commit to starting sample project

---
+++ controller/src/main/java/br/com/farzac/opendaylight/controller/App.java
@@ -0,0 +1,13 @@
+package br.com.farzac.opendaylight.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
